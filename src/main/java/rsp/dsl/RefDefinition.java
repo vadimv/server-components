ed4f8b1d162b38267b9a879ed88fdbe701854abd
Vadim Vashkevich <vadim.vashkevich@gmail.com>
2020-09-17 20:36:25 +0200

    Implement references with keys

--- src/main/java/rsp/dsl/RefDefinition.java
+++ src/main/java/rsp/dsl/RefDefinition.java
@@ -3,12 +3,20 @@ package rsp.dsl;
 import rsp.EventContext;
 import rsp.RenderContext;
 
+import java.util.Objects;
 import java.util.function.Consumer;
 
-public class RefDefinition<S> extends DocumentPartDefinition {
-
+public class RefDefinition extends DocumentPartDefinition {
+    private final Object parentRef;
+    private final Object key;
     public RefDefinition() {
+        this(new Object(), new Object());
+    }
+
+    private RefDefinition(Object parentRef, Object key) {
         super(DocumentPartKind.OTHER);
+        this.parentRef = parentRef;
+        this.key = key;
     }
 
     @Override
@@ -16,4 +24,21 @@ public class RefDefinition<S> extends DocumentPartDefinition {
         renderContext.addRef(this);
     }
 
+    public RefDefinition withKey(Object key) {
+        return new RefDefinition(this, key);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RefDefinition that = (RefDefinition) o;
+        return parentRef.equals(that.parentRef) &&
+                key.equals(that.key);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(parentRef, key);
+    }
 }
