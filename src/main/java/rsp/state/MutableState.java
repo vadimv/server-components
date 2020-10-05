46d399e9aa87e9f34a9a1125d130986599f8ec88
Vadim Vashkevich <vadim.vashkevich@gmail.com>
2020-10-05 20:00:16 +0200

    Add a state update event impl

--- src/main/java/rsp/state/MutableState.java
+++ src/main/java/rsp/state/MutableState.java
@@ -1,10 +1,18 @@
 package rsp.state;
 
+import java.util.Arrays;
+
 public class MutableState<S> implements UseState<S> {
+    private final StateListener<S>[] listeners;
     private volatile S state;
 
     public MutableState(S initialState)  {
+        this(initialState, new StateListener[] {});
+    }
+
+    public MutableState(S initialState, StateListener<S>... listeners)  {
         this.state = initialState;
+        this.listeners = listeners;
     }
 
     @Override
@@ -14,6 +22,20 @@ public class MutableState<S> implements UseState<S> {
 
     @Override
     public void accept(S state) {
+        final S oldState = this.state;
         this.state = state;
+        for (StateListener<S> listener:listeners) {
+            listener.onNewState(oldState, state);
+        }
+    }
+
+    public MutableState<S> addListener(StateListener<S> stateListener) {
+        final StateListener[] a = Arrays.copyOf(listeners, listeners.length + 1);
+        a[a.length - 1] = stateListener;
+        return new MutableState<>(state, a);
+    }
+
+    public interface StateListener<S> {
+        void onNewState(S oldState, S newState);
     }
 }
