dc35ffe60db10acf6c06ce8df40089fb09a5e0bc
Vadim Vashkevich <vadim.vashkevich@gmail.com>
2020-10-21 21:00:24 +0200

    Fix a remove attribute command's namespace parameter

--- src/main/java/rsp/server/SerializeKorolevOutMessages.java
+++ src/main/java/rsp/server/SerializeKorolevOutMessages.java
@@ -98,7 +98,7 @@ public class SerializeKorolevOutMessages implements OutMessages {
     private String modifyDomMessageBody(DomChange domChange) {
         if (domChange instanceof RemoveAttr) {
             final RemoveAttr c = (RemoveAttr)domChange;
-            return joinString(REMOVE_ATTR, quote(c.path), quote(c.xmlNs), quote(escape(c.name)), false);
+            return joinString(REMOVE_ATTR, quote(c.path), xmlNsString(c.xmlNs), quote(escape(c.name)), false);
         } else if (domChange instanceof RemoveStyle) {
             final RemoveStyle c = (RemoveStyle)domChange;
             return joinString(REMOVE_STYLE, quote(c.path), quote(escape(c.name)), false);
@@ -124,7 +124,7 @@ public class SerializeKorolevOutMessages implements OutMessages {
     }
 
     private String xmlNsString(XmlNs xmlNs) {
-        return xmlNs.uri.equals(XmlNs.html.uri) ? "0" : xmlNs.toString();
+        return xmlNs.uri.equals(XmlNs.html.uri) ? "0" : quote(xmlNs.toString());
     }
 
     @Override
