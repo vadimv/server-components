0c7a33a43b2f4e44c0040d3f98c5f01dd343db1a
Vadim Vashkevich <vadim.vashkevich@gmail.com>
2020-11-01 08:21:17 +0100

    Add stub implementation of the menu panel component

--- src/main/java/rsp/examples/crud/components/MenuPanel.java
+++ src/main/java/rsp/examples/crud/components/MenuPanel.java
@@ -1,4 +1,19 @@
 package rsp.examples.crud.components;
 
+import rsp.Component;
+
+import static rsp.dsl.Html.*;
+
 public class MenuPanel {
+
+    public static final Component<MenuPanelState> component = state ->
+            div(
+                ul(
+                    li("first"),
+                    li("second")
+            ));
+
+    public static class MenuPanelState {
+
+    }
 }
