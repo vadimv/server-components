4931cd0a13e24e1c2acdf9499e98abe7a690e091
Vadim Vashkevich <vadim.vashkevich@gmail.com>
2020-12-28 22:45:13 +0100

    Refactor

--- src/main/java/rsp/Component.java
+++ src/main/java/rsp/Component.java
@@ -4,6 +4,6 @@ import rsp.dsl.DocumentPartDefinition;
 import rsp.state.UseState;
 
 @FunctionalInterface
-public interface  Component<S> {
+public interface Component<S> {
     DocumentPartDefinition render(UseState<S> us);
 }
