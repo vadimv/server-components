70d2e7bbca16c0e849e9314f413e9c0091b7a5d2
Vadim Vashkevich <vadim.vashkevich@gmail.com>
2021-01-09 10:11:19 +0100

    Add a Javadoc

--- src/main/java/rsp/state/CompletableFutureConsumer.java
+++ src/main/java/rsp/state/CompletableFutureConsumer.java
@@ -6,6 +6,12 @@ import java.util.concurrent.CompletableFuture;
  * Represents a write state snapshot operation using a {@link CompletableFuture}.
  * @param <S> the type of the state snapshot, an immutable class
  */
+@FunctionalInterface
 public interface CompletableFutureConsumer<S> {
+
+    /**
+     * Performs this write operation when the argument {@link CompletableFuture} completes with its result
+     * @param completableFuture a computation resulting in a write
+     */
     void accept(CompletableFuture<S> completableFuture);
 }
