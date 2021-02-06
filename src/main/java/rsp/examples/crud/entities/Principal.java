90f96ce15d5596b768cb27baff46dcc01c95d315
Vadim Vashkevich <vadim.vashkevich@gmail.com>
2021-02-06 10:25:33 +0100

    Remove the CRUD example

--- src/main/java/rsp/examples/crud/entities/Principal.java
+++
@@ -1,9 +0,0 @@
-package rsp.examples.crud.entities;
-
-public class Principal {
-    public final String name;
-
-    public Principal(String name) {
-        this.name = name;
-    }
-}
--- src/main/java/rsp/examples/crud/services/EntityService.java
+++
@@ -1,4 +0,0 @@
-package rsp.examples.crud.services;
-
-public interface EntityService<K, T> extends GetOne<K, T>, GetList<K, T>, Create<K, T>, Delete<K, T>, Update<K, T> {
-}
