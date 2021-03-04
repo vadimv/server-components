414935dd1c8e2348e7724b82d60134f734528787
Vadim Vashkevich <vadim.vashkevich@gmail.com>
2021-03-04 19:18:00 +0100

    Update Javadocs

--- src/main/java/rsp/AppConfig.java
+++ src/main/java/rsp/AppConfig.java
@@ -15,12 +15,12 @@ public final class AppConfig {
     public final static String LOG_LEVEL_PROPERTY_NAME = "rsp.log.level";
 
     /**
-     * The default log level
+     * The default log level.
      */
     public final static Log.Level DEFAULT_LOG_LEVEL = Log.Level.INFO;
 
     /**
-     * A console logger used by default
+     * A console logger used by default.
      */
     public final static Log.Reporting DEFAULT_CONSOLE_LOG = new Log.Default(Log.Level.valueOf(System.getProperty(LOG_LEVEL_PROPERTY_NAME,
                                                                                                                  DEFAULT_LOG_LEVEL.name()).toUpperCase()),
@@ -28,39 +28,39 @@ public final class AppConfig {
                                                                                                string -> System.out.println(string));
 
     /**
-     * The default rate of heartbeat messages from a browser to server
+     * The default rate of heartbeat messages from a browser to server.
      */
     public static final int DEFAULT_HEARTBEAT_INTERVAL_MS = 10000;
 
     /**
-     * The application's scheduler thread pool default size
+     * The application's scheduler thread pool default size.
      */
     public static final int DEFAULT_SCHEDULER_THREAD_POOL_SIZE = 10;
 
     /**
-     * The default configuration
+     * The default configuration.
      */
     public static AppConfig DEFAULT = new AppConfig(DEFAULT_HEARTBEAT_INTERVAL_MS,
                                                     DEFAULT_SCHEDULER_THREAD_POOL_SIZE,
                                                     DEFAULT_CONSOLE_LOG);
 
     /**
-     * The rate of heartbeat messages from a browser to server
+     * The rate of heartbeat messages from a browser to server.
      */
     public final int heartbeatIntervalMs;
 
     /**
-     * The application's scheduler thread pool size
+     * The application's scheduler thread pool size.
      */
     public final int schedulerThreadPoolSize;
 
     /**
-     * The application's logger
+     * The application's logger.
      */
     public final Log.Reporting log;
 
     /**
-     * Creates an instance of an application object
+     * Creates an instance of an application object.
      * @param heartbeatIntervalMs the rate of heartbeat messages from a browser to server
      * @param schedulerThreadPoolSize the application's scheduler thread pool size
      * @param log the application's logger
@@ -74,7 +74,7 @@ public final class AppConfig {
     }
 
     /**
-     * Creates a new copy of the configuration with a provided heartbeat interval
+     * Creates a new copy of the configuration with a provided heartbeat interval.
      * @param heartbeatIntervalMs the rate of heartbeat messages from a browser to server
      * @return a new configuration object with the same field values except of the provided field
      */
@@ -85,7 +85,7 @@ public final class AppConfig {
     }
 
     /**
-     * Creates a new copy of the configuration with a provided application logger
+     * Creates a new copy of the configuration with a provided application logger.
      * @param log the application's logger
      * @return a new configuration object with the same field values except of the provided field
      */
