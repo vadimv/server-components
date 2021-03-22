bc644777d217b33b19b1846065828ee168e5bb73
Vadim Vashkevich <vadim.vashkevich@gmail.com>
2021-03-22 20:09:28 +0100

    Make some changes on the plain form example

--- src/main/java/rsp/examples/PlainForm.java
+++ src/main/java/rsp/examples/PlainForm.java
@@ -1,6 +1,7 @@
 package rsp.examples;
 
 import rsp.App;
+import rsp.Component;
 import rsp.jetty.JettyServer;
 
 import static rsp.dsl.Html.*;
@@ -8,25 +9,55 @@ import static rsp.dsl.Html.*;
 public class PlainForm {
     public static void main(String[] args) throws Exception {
         final var app = new App<>("Hello world!",
-                                  s -> of(() -> html(
-                    head(UpgradeMode.RAW, title("Plain form")),
-                    body(
-                         h2(text("HTML Forms")),
-                            form(attr("action", "page0"), attr("method", "post"),
-                                 label(attr("for", "firstname"), text("First name:")),
-                                 input(attr("type", "text"), attr("name","firstname"), attr("value", "First")),
-                                 br(),
-                                 label(attr("for", "lastname"), text("Last name:")),
-                                 input(attr("type", "text"), attr("name","lastname"), attr("value", "Last")),
-                                 br(),
-                                 input(attr("type", "submit"), attr("value", "Submit"))
-                            ),
-                            p("If you click the 'Submit' button, the form-data will be sent to page0.")
-                    )
-            )
-        ));
+                                  pages());
         final var server = new JettyServer(8080, "", app);
         server.start();
         server.join();
+    }
+
+    private static Component<String> pages() {
+        return s -> of(() -> html(
+                head(UpgradeMode.RAW, title("Page: " + s.get())),
+                body(
+                    formComponent().render(s)
+                )
+            )
+        );
+    }
+
+/*    private static Component<String> page(String name) {
+        return  formComponent().render(s);
+    }*/
+
+    private static Component<String> formComponent() {
+        return s -> div(
+                h2(text("HTML Form")),
+                form(attr("action", "page0"), attr("method", "post"),
+                label(attr("for", "firstname"), text("First name:")),
+                input(attr("type", "text"), attr("name","firstname"), attr("value", "First")),
+                br(),
+                label(attr("for", "lastname"), text("Last name:")),
+                input(attr("type", "text"), attr("name","lastname"), attr("value", "Last")),
+                br(),
+                input(attr("type", "submit"), attr("value", "Submit"))),
+                p("If you click the 'Submit' button, the form-data will be sent to page0."));
+    }
+
+    private static Component<String> formResult() {
+        return s -> div(
+                h2(text("HTML Form result")),
+                form(attr("action", "page0"), attr("method", "post"),
+                        label(attr("for", "firstname"), text("First name:")),
+                        input(attr("type", "text"), attr("name","firstname"), attr("value", "First")),
+                        br(),
+                        label(attr("for", "lastname"), text("Last name:")),
+                        input(attr("type", "text"), attr("name","lastname"), attr("value", "Last")),
+                        br(),
+                        input(attr("type", "submit"), attr("value", "Submit"))),
+                p("If you click the 'Submit' button, the form-data will be sent to page0."));
+    }
+
+    private static String onSubmit() {
+        return "";
     }
 }
