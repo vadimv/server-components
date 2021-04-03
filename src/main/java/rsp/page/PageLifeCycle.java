b743bce66eceaadc1fbd9ee54982b306b8fc09d2
Vadim Vashkevich <vadim.vashkevich@gmail.com>
2021-04-03 08:46:54 +0200

    Add a page event listener, add Javadocs

--- src/main/java/rsp/page/PageLifeCycle.java
+++ src/main/java/rsp/page/PageLifeCycle.java
@@ -1,11 +1,27 @@
 package rsp.page;
 
+/**
+ * The listener interface for receiving page lifecycle events.
+ * @param <S>
+ */
 public interface PageLifeCycle<S> {
 
+    /**
+     * Invoked before an live page session created.
+     * @param state
+     */
     void beforeLivePageCreated(S state);
 
+    /**
+     * Invoked after an live page session closed.
+     * @param state
+     */
     void afterLivePageClosed(S state);
 
+    /**
+     * The default lifecycle listener implementation doing nothing.
+     * @param <S>
+     */
     class Default<S> implements PageLifeCycle<S> {
 
         @Override
