93713e4d63dd2e7e26ce01f3975c62400c798ae5
Vadim Vashkevich <vadim.vashkevich@gmail.com>
2021-05-29 16:24:52 +0200

    Refactor the plain form example

--- src/main/java/rsp/examples/PlainForm.java
+++ src/main/java/rsp/examples/PlainForm.java
@@ -5,6 +5,8 @@ import rsp.Component;
 import rsp.jetty.JettyServer;
 import rsp.server.HttpRequest;
 
+import java.util.Objects;
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 
 import static rsp.dsl.Html.*;
@@ -15,31 +17,44 @@ import static rsp.dsl.Html.*;
  */
 public class PlainForm {
     public static void main(String[] args) {
-        final var app = new App<>(PlainForm::routes,
-                                  pages());
-        final var server = new JettyServer(8080, "", app);
+        final App<Optional<FullName>> app = new App<>(PlainForm::routes,
+                                                      pages());
+        final JettyServer server = new JettyServer(8080, "", app);
         server.start();
         server.join();
     }
 
-    private static CompletableFuture<String> routes(HttpRequest r) {
-        return r.method.equals(HttpRequest.Methods.POST) ?
-                CompletableFuture.completedFuture(r.param("firstname").orElseThrow()
-                        + " "
-                        + r.param("lastname").orElseThrow())
-                : CompletableFuture.completedFuture("");
+    public static class FullName {
+        public final String firstName;
+        public final String secondName;
+
+        public FullName(String firstName, String secondName) {
+            this.firstName = Objects.requireNonNull(firstName);
+            this.secondName = Objects.requireNonNull(secondName);
+        }
+
+        public String toString() {
+            return firstName + " " + secondName;
+        }
     }
 
-    private static Component<String> pages() {
+    private static CompletableFuture<Optional<FullName>> routes(HttpRequest r) {
+        return r.method.equals(HttpRequest.Methods.POST) ?
+                CompletableFuture.completedFuture(Optional.of(new FullName(r.param("firstname").orElseThrow(),
+                                                                           r.param("lastname").orElseThrow())))
+                : CompletableFuture.completedFuture(Optional.empty());
+    }
+
+    private static Component<Optional<FullName>> pages() {
         return s -> html(
                         headPlain(title("Plain Form Pages")),
                         body(
-                            "".equals(s.get()) ? formComponent().render(s) : formResult().render(s)
+                            s.get().isEmpty() ? formComponent().render(s) : formResult().render(s)
                         )
         );
     }
 
-    private static Component<String> formComponent() {
+    private static Component<Optional<FullName>> formComponent() {
         return s -> div(
                 h2(text("HTML Form")),
                 form(attr("action", "page0"), attr("method", "post"),
@@ -53,9 +68,8 @@ public class PlainForm {
                 p("If you click the 'Submit' button, the form-data will be sent to page0."));
     }
 
-    private static Component<String> formResult() {
-        return s -> div(
-                h2(text("HTML Form result")),
-                div(p("The submitted name is " + s.get())));
+    private static Component<Optional<FullName>> formResult() {
+        return s -> div(h2(text("HTML Form result")),
+                        div(p("The submitted name is " + s.get().orElseThrow())));
     }
 }
