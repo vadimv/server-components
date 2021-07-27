0e3e14927694b9f40f42ac8615dfe030ea5806ce
Vadim Vashkevich <vadim.vashkevich@gmail.com>
2021-07-27 18:57:02 +0200

    Add the i18n package and base interfaces

---
+++ src/main/java/rsp/i18n/LocaleSupply.java
@@ -0,0 +1,7 @@
+package rsp.i18n;
+
+import java.util.Locale;
+
+public interface LocaleSupply {
+    Locale locale();
+}
---
+++ src/main/java/rsp/i18n/Translate.java
@@ -0,0 +1,5 @@
+package rsp.i18n;
+
+public interface Translate {
+    String t(String key);
+}
