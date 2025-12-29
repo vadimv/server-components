12df76a0f124373023033c10cb7a9643011c9e34
vadimv <vadim.vashkevich@gmail.com>
2025-12-29 19:12:18 +0100

    Update JavaDocs, cleanup

--- core/src/main/java/rsp/util/RandomString.java
+++ core/src/main/java/rsp/util/RandomString.java
@@ -2,6 +2,9 @@ package rsp.util;
 
 import java.security.SecureRandom;
 
+/**
+ * A generator of random alfa-numeric strings.
+ */
 public final class RandomString {
 
     private static final String ALPHABET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
@@ -10,10 +13,18 @@ public final class RandomString {
 
     private final SecureRandom rnd = new SecureRandom();
 
+    /**
+     * Creates a new instance of a fixed length random string generator.
+     * @param length the length of the generated strings
+     */
     public RandomString(final int length) {
         this.length = length;
     }
 
+    /**
+     * Generates a new random alfa-numeric string.
+     * @return a random string of a fixed length
+     */
     public String newString() {
         final StringBuilder sb = new StringBuilder(length);
         var i = 0;
